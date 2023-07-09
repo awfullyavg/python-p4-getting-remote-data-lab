a3ca373606fb1f2178185f292ef4079517e1c618
Bryant Wiggins <topwigit@gmail.com>
2023-07-08 20:13:31 -0400

    think its a soltuion

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,16 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
+    
     def get_response_body(self):
-        pass
+        response_body = requests.get(self.url)
+        return response_body.content
 
     def load_json(self):
-        pass
+        return json.load(self.get_response_body())
+            
+        
+
